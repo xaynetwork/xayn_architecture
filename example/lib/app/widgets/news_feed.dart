5cc65a9b9d2bf7e898fcc941c79bc5afb3afe529
Peter Stojanowski <peter.stojanowski@gmail.com>
2021-12-15 14:21:36 +0000

    Flutter 2.8.0 upgrade

--- example/lib/app/widgets/news_feed.dart
+++ example/lib/app/widgets/news_feed.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:xayn_architecture_example/app/constants/r.dart';
 import 'package:xayn_architecture_example/app/managers/news_feed_manager.dart';
