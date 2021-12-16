2016404a511bb6232d32486e25d4f6b8c46a9c36
Simon Joecks <simon.joecks@xayn.com>
2021-12-16 10:19:52 +0100

    Add routing source so that it is possible to know from where a route was pushed

--- lib/concepts/navigation/navigator_state.dart
+++ lib/concepts/navigation/navigator_state.dart
@@ -1,16 +1,41 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:xayn_architecture/concepts/navigation/navigator_manager.dart';
 import 'package:xayn_architecture/concepts/navigation/page_data.dart';
+
+/// A source that describes by what event the State was created.
+enum Source {
+  /// Unknown source. Mainly used until we can determine the source.
+  unknown,
+
+  /// Routes created internally by the [NavigatorManager] itself
+  navigator,
+
+  /// Route that is pushed from an external source (like the browser Nav bar or a deep-link)
+  external,
+
+  /// Route that is created during the state restoration
+  restoration,
+
+  /// Route that is pushed on the creation of the widget.
+  /// The responsible routeName for the initial route is: [_WidgetsAppState._initialRouteName]
+  initialization,
+}
 
 /// The [NavigatorState] describes which pages will be rendered in the [Navigator] Widget.
 class NavigatorState extends Equatable {
   /// see [NavigatorState] constructor
   final List<UntypedPageData> pages;
 
+  /// The source from where the state was pushed from
+  final Source source;
+
   /// - [pages] represent the pages that are currently available in the Navigator backstack.
-  NavigatorState({required List<UntypedPageData> pages})
-      : pages = List.unmodifiable(pages);
+  NavigatorState({
+    required List<UntypedPageData> pages,
+    this.source = Source.unknown,
+  }) : pages = List.unmodifiable(pages);
 
   @override
-  List<Object?> get props => [pages];
+  List<Object?> get props => [pages, source];
 }
